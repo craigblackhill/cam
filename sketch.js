0d456349ce9b510794c35e07a9be1b55ed05925c
craigblackhill <craigblackhill@gmail.com>
2021-10-11 16:36:54 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,20 +22,20 @@ function draw() {
       var p = img.pixels[(y*img.width-x)*4];
       if (p < 255 & p > 200 )
       {
-        fill(p*2,p,p*3);
+        fill(0,p,0);
 				
-		 ellipse(x, y, 11, 11);
+		 ellipse(x, y, 22, 22);
       }
            if (p < 200 & p > 100 )
       {
-        fill(22,p,p);
-       ellipse(x, y, 22, 22);
+        fill(0,p,p);
+       ellipse(x, y, 33, 33);
 
       }
 			          if (p < 100 & p > 0 )
       {
-        fill(220,p,222);
-       ellipse(x, y, 6, 6);
+        fill(0,p,0);
+       ellipse(x, y, 44, 44);
 
       }
     }
