4e84be8cd8f915b249d065568828f7689e3f3250
craigblackhill <craigblackhill@gmail.com>
2021-10-11 16:46:13 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,17 @@
-function setup() { 
-  createCanvas(displayWidth, displayHeight);
-  img = createCapture(VIDEO);
-	img.hide();
 
-
-}
 function mousePressed() {
 
     let fs = fullscreen();
 
     fullscreen(!fs); 
 }
+function setup() { 
+  createCanvas(400, 300);
+  img = createCapture(VIDEO);
+	img.hide();
+  img.size(800, 600);
 
-
+}
 function draw() {
 	img.loadPixels();
   for (var y=img.height ; y>0; y-=10)
@@ -22,20 +21,20 @@ function draw() {
       var p = img.pixels[(y*img.width-x)*4];
       if (p < 255 & p > 200 )
       {
-        fill(0,p,0);
+        fill(p*2,p,p*3);
 				
-		 ellipse(x, y, 22, 22);
+		 ellipse(x, y, 11, 11);
       }
            if (p < 200 & p > 100 )
       {
-        fill(0,p,p);
-       ellipse(x, y, 33, 33);
+        fill(22,p,p);
+       ellipse(x, y, 22, 22);
 
       }
 			          if (p < 100 & p > 0 )
       {
-        fill(0,p,0);
-       ellipse(x, y, 44, 44);
+        fill(220,p,222);
+       ellipse(x, y, 6, 6);
 
       }
     }
