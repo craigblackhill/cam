51ea8d11f0b83dca28c69f4a3a1857c3d514a4c9
craigblackhill <craigblackhill@gmail.com>
2021-10-10 18:32:49 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,35 +1,93 @@
-function setup() { 
-  createCanvas(400, 300);
-  img = createCapture(VIDEO);
-	img.hide();
-  img.size(800, 600);
+let gra;
+let url = "https://coolors.co/f7f7f7-527caa-f76954-ffe62d-5fb49c";
+let cols;
 
+let deltaSec = 0;
+let rote = 0;
+let cap;
+
+
+function setup() {
+	createCanvas(1112, 834);
+	cap = createCapture(VIDEO);
+	cap.hide();
+	cols = createCols(url);	
+	init();
 }
-function draw() {
-	img.loadPixels();
-  for (var y=img.height ; y>0; y-=10)
-  {
-    for (var x=img.width; x>0; x-=10)
-    {
-      var p = img.pixels[(y*img.width-x)*4];
-      if (p < 255 & p > 200 )
-      {
-        fill(p*2,p,p*3);
-				
-		 ellipse(x, y, 11, 11);
-      }
-           if (p < 200 & p > 100 )
-      {
-        fill(22,p,p);
-       ellipse(x, y, 22, 22);
 
-      }
-			          if (p < 100 & p > 0 )
-      {
-        fill(220,p,222);
-       ellipse(x, y, 6, 6);
+function draw()
+{
+	rote += 0.0025 + sin(frameCount/100)*0.001;
+	
+	//reset layout
+	let pSec = deltaSec;
+	deltaSec = Math.floor(millis()/1000);
+	if(pSec != deltaSec && deltaSec % 5 == 0)init();
+	
+	//gra
+	let triH = gra.width*0.5 * pow(3,0.5);
 
-      }
-    }
+	gra.clear();
+	gra.background(0);
+	gra.push();
+	gra.translate(gra.width/2,triH/2);
+	gra.rotate(rote);
+	gra.imageMode(CENTER);
+	let ratio = gra.width/min(cap.width,cap.height)*1.5;
+	gra.image(cap,0,0,cap.width*ratio,cap.height*ratio);	
+	gra.pop();
+	
+	//mask
+	gra.erase();
+	gra.triangle(0,triH,0,0,gra.width/2,triH);
+	gra.triangle(gra.width,triH,gra.width,0,gra.width/2,triH);
+	gra.rect(0,triH,gra.width,gra.height-triH);
+	gra.noErase();
+	
+	
+	// tiling gra
+	clear();
+	let offY = 0; 
+	let size = dist(0,0,width,height);
+	translate(width/2,height/2);
+	rotate(rote);
+	for(let x = -size/2-gra.width; x < size/2 + gra.width; x += gra.width*1.5)
+	{
+		for(let y = -size/2-triH; y < size/2 + triH; y+= triH*2){drawHexa(x,y+offY);}
+		offY = offY == 0 ? triH : 0;
+	}
+}
+
+
+function init(){
+	let graSize = Math.floor(max(width,height)/random(3,15));
+	gra = createGraphics(graSize,graSize);
+	gra.noStroke();
+}
+
+function drawHexa(cx,cy,triGra = gra,radius = gra.width)
+{	
+	push();
+	translate(cx,cy);
+
+	for(let rad = 0; rad < TWO_PI; rad += TWO_PI/3)
+	{
+		push();
+		rotate(rad);
+		image(triGra,0,0);
+		scale(1,-1);
+		image(triGra,0,0);
+		pop();
+	}
+	pop();
+}
+
+function createCols(_url) {
+  let slash_index = _url.lastIndexOf('/');
+  let pallate_str = _url.slice(slash_index + 1);
+  let arr = pallate_str.split('-');
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = '#' + arr[i];
   }
+  return arr;
 }
