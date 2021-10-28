bf074343ef809021dced404282975e46f0e52582
craigblackhill <craigblackhill@gmail.com>
2021-10-27 20:51:27 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,10 +5,10 @@ function mousePressed() {
     fullscreen(!fs); 
 }
 function setup() { 
-  createCanvas(700, 600);
+  createCanvas(400, 300);
   img = createCapture(VIDEO);
 	img.hide();
-
+  img.size(800, 600);
 
 }
 function draw() {
@@ -22,18 +22,18 @@ function draw() {
       {
         fill(p*2,p,p*3);
 				
-		 ellipse(x, y, 22, 22);
+		 ellipse(x, y, 11, 11);
       }
            if (p < 200 & p > 100 )
       {
-        fill(p,255,p);
-       ellipse(x, y, 44, 44);
+        fill(22,p,p);
+       ellipse(x, y, 22, 22);
 
       }
 			          if (p < 100 & p > 0 )
       {
-        fill(0,p,0);
-       ellipse(x, y, 12, 12);
+        fill(220,p,222);
+       ellipse(x, y, 6, 6);
 
       }
     }
